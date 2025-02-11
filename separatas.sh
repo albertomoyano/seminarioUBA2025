cad60940864c88e60632ebfa2691717f8b8e0baf
alberto <alberto.alejandro.moyano@gmail.com>
2025-02-11 18:25:03 -0300

    11-02-2025

--- separatas.sh
+++ separatas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-pdftk ./pdf/epub1-seminarioUBA.pdf cat 8-10 output ./docs/prologo-epub1-seminarioUBA.pdf
-pdftk ./pdf/epub1-seminarioUBA.pdf cat 11-13 output ./docs/capitulo1-epub1-seminarioUBA.pdf
-pdftk ./pdf/epub1-seminarioUBA.pdf cat 15-18 output ./docs/capitulo2-epub1-seminarioUBA.pdf
-pdftk ./pdf/epub1-seminarioUBA.pdf cat 19-21 output ./docs/capitulo3-epub1-seminarioUBA.pdf
-pdftk ./pdf/epub1-seminarioUBA.pdf cat 22-24 output ./docs/referencias-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 5-7 output ./docs/prologo-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 8-11 output ./docs/capitulo1-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 12-15 output ./docs/capitulo2-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 16-19 output ./docs/capitulo3-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 20-22 output ./docs/referencias-epub1-seminarioUBA.pdf
 exit
