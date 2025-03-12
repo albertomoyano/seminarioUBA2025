1484c950c635b3da134e47d4aa5c1474901b8adf
alberto <alberto.alejandro.moyano@gmail.com>
2025-03-12 14:53:47 -0300

    12-03-2025

--- separatas.sh
+++ separatas.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-pdftk ./pdf/pdf-seminarioUBA.pdf cat 9-11 output ./docs/presentacion-pdf-seminarioUBA.pdf
-pdftk ./pdf/pdf-seminarioUBA.pdf cat 13-15 output ./docs/prologo-pdf-seminarioUBA.pdf
-pdftk ./pdf/pdf-seminarioUBA.pdf cat 17-19 output ./docs/introduccion-pdf-seminarioUBA.pdf
-pdftk ./pdf/pdf-seminarioUBA.pdf cat 21-23 output ./docs/capitulo1-pdf-seminarioUBA.pdf
-pdftk ./pdf/pdf-seminarioUBA.pdf cat 25-27 output ./docs/capitulo2-pdf-seminarioUBA.pdf
-pdftk ./pdf/pdf-seminarioUBA.pdf cat 29-32 output ./docs/capitulo3-pdf-seminarioUBA.pdf
-pdftk ./pdf/pdf-seminarioUBA.pdf cat 33-34 output ./docs/conclusiones-pdf-seminarioUBA.pdf
-pdftk ./pdf/pdf-seminarioUBA.pdf cat 35-46 output ./docs/indices-pdf-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 5-7 output ./docs/presentacion-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 8-10 output ./docs/prologo-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 11-13 output ./docs/introduccion-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 14-17 output ./docs/capitulo1-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 18-21 output ./docs/capitulo2-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 22-26 output ./docs/capitulo3-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 27-28 output ./docs/conclusiones-epub1-seminarioUBA.pdf
+pdftk ./pdf/epub1-seminarioUBA.pdf cat 29-35 output ./docs/indices-epub1-seminarioUBA.pdf
 exit
