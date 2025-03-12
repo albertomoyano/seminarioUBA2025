da44aaac90d8bcea401b007b1c4f2437aff65937
alberto <alberto.alejandro.moyano@gmail.com>
2025-03-11 22:59:09 -0300

    11-03-2025

--- separatas.sh
+++ separatas.sh
@@ -5,5 +5,6 @@ pdftk ./pdf/pdf-seminarioUBA.pdf cat 17-19 output ./docs/introduccion-pdf-semina
 pdftk ./pdf/pdf-seminarioUBA.pdf cat 21-23 output ./docs/capitulo1-pdf-seminarioUBA.pdf
 pdftk ./pdf/pdf-seminarioUBA.pdf cat 25-27 output ./docs/capitulo2-pdf-seminarioUBA.pdf
 pdftk ./pdf/pdf-seminarioUBA.pdf cat 29-32 output ./docs/capitulo3-pdf-seminarioUBA.pdf
-pdftk ./pdf/pdf-seminarioUBA.pdf cat 37-40 output ./docs/referencias-pdf-seminarioUBA.pdf
+pdftk ./pdf/pdf-seminarioUBA.pdf cat 33-34 output ./docs/conclusiones-pdf-seminarioUBA.pdf
+pdftk ./pdf/pdf-seminarioUBA.pdf cat 35-44 output ./docs/referencias-pdf-seminarioUBA.pdf
 exit
