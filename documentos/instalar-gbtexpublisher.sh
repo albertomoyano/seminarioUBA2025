1706b8a181c06dbab51844195bb0faa3af58be9b
alberto <alberto.alejandro.moyano@gmail.com>
2025-04-25 12:48:06 -0300

    25-04-2025

--- documentos/instalar-gbtexpublisher.sh
+++ documentos/instalar-gbtexpublisher.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Lista de componentes que necesita tu programa
-# Puedes modificar esta lista según los que realmente use tu app
 COMPONENTES=(
     gambas3-runtime
     gambas3-gb-db
@@ -11,22 +10,21 @@ COMPONENTES=(
     gambas3-gb-form
     gambas3-gb-form-dialog
     gambas3-gb-form-terminal
-	gambas3-gb-image
-	gambas3-gb-inotify
-	gambas3-gb-net
-	gambas3-gb-net-curl
-	gambas3-gb-qt5
-	gambas3-gb-qt5-ext
-	gambas3-gb-qt5-webview
-	gambas3-gb-settings
-	gambas3-gb-term
-	gambas3-gb-util
-	gambas3-gb-util-web
+    gambas3-gb-image
+    gambas3-gb-inotify
+    gambas3-gb-net
+    gambas3-gb-net-curl
+    gambas3-gb-qt5
+    gambas3-gb-qt5-ext
+    gambas3-gb-qt5-webview
+    gambas3-gb-settings
+    gambas3-gb-term
+    gambas3-gb-util
+    gambas3-gb-util-web
 )
 
 echo "Detectando distribución..."
 
-# Detectar distribución
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     DISTRO=$ID
@@ -35,7 +33,6 @@ else
     exit 1
 fi
 
-# Función para instalar componentes si no están instalados
 instalar_si_faltan() {
     local comando_check=$1
     local comando_instalar=$2
@@ -50,35 +47,50 @@ instalar_si_faltan() {
     done
 }
 
-# Instalar según distribución
 case "$DISTRO" in
     ubuntu|debian|linuxmint)
         echo "Distribución Debian/Ubuntu/Mint detectada."
-        apt update
-        instalar_si_faltan "dpkg -s" "sudo apt install -y"
+        apt update &>/dev/null || echo "[ADVERTENCIA] Falló apt update (se ignora)..."
+        instalar_si_faltan "dpkg -s" "apt install -y"
         ;;
 
     opensuse*|suse)
         echo "Distribución openSUSE detectada."
-        instalar_si_faltan "rpm -q" "sudo zypper install -y"
+        instalar_si_faltan "rpm -q" "zypper install -y"
         ;;
 
     fedora)
         echo "Distribución Fedora detectada."
-        instalar_si_faltan "rpm -q" "sudo dnf install -y"
+        instalar_si_faltan "rpm -q" "dnf install -y"
         ;;
 
     arch|manjaro)
         echo "Distribución Arch/Manjaro detectada."
-        instalar_si_faltan "pacman -Q" "sudo pacman -S --noconfirm"
+        instalar_si_faltan "pacman -Q" "pacman -S --noconfirm"
         ;;
 
     *)
-        echo "Distribución $DISTRO no soportada automáticamente."
-        echo "Por favor instala manualmente los siguientes componentes:"
+        echo "Distribución $DISTRO no soportada."
         printf '%s\n' "${COMPONENTES[@]}"
         exit 1
         ;;
 esac
 
-echo "Todos los componentes de Gambas han sido verificados e instalados según necesidad."
+echo "Componentes de Gambas verificados."
+
+if [[ "$DISTRO" =~ (ubuntu|debian|linuxmint) ]]; then
+    DEB_URL="https://github.com/albertomoyano/gbtexpublisher/releases/download/deb-2025.04.19/gbtexpublisher0_0.1.662-0ubuntu1_all.deb"
+    DEB_FILE="gbtexpublisher.deb"
+    
+    echo "Descargando gbtexpublisher..."
+    if wget "$DEB_URL" -O "$DEB_FILE"; then
+        echo "Instalando..."
+        dpkg -i "./$DEB_FILE" || apt install -f -y
+        rm -f "$DEB_FILE"
+    else
+        echo "Error al descargar."
+        exit 1
+    fi
+else
+    echo "Solo disponible para Debian/Ubuntu/Mint."
+fi
